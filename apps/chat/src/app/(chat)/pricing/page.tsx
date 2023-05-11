ab23a0a2751be5ffe93628ac7a308ad305f35d05
leozhou <492753662@qq.com>
2023-05-11 20:42:56 +0800

    feat: update color and disclaimer” “

--- apps/chat/src/app/(chat)/pricing/page.tsx
+++ apps/chat/src/app/(chat)/pricing/page.tsx
@@ -119,8 +119,15 @@ export default function PricingPage() {
     <>
       <div className={styles["window-header"]}>
         <div className={styles["window-header-title"]}>
-          <div className={styles["window-header-main-title"]}>定价</div>
-          <div className={styles["window-header-sub-title"]}>解锁更多权益</div>
+          <div className={styles["window-header-main-title"]}>免责说明</div>
+          <div className={styles["window-header-sub-title"]}>本站永不提供任何上网的技术和软件教程
+本站仅供免费用于学习测试，请勿用于任何违法活动行为
+开发难度大，希望你可以进群支持一下我，或者直接打个款开心一下
+禁止发布、传播任何违法、违规内容，使用本网站，视您接受并同意《免责声明》
+服务器昂贵,接口昂贵,但网站免费！！底下输入框输入就能直接用～
+如果你觉得对你有帮助可以进群或者打赏让服务器活的久一点
+每人每月捐个三元、服务就能永久免费下去！！
+</div>
         </div>
         <div className={styles["window-actions"]}>
           <div className={styles["window-action-button"]}>

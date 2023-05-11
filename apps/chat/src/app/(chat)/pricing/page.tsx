b0be2b68855bfffdcebf1e7f155cbfb9ec283c59
leozhou <492753662@qq.com>
2023-05-11 21:40:17 +0800

    feat: update plan desc

--- apps/chat/src/app/(chat)/pricing/page.tsx
+++ apps/chat/src/app/(chat)/pricing/page.tsx
@@ -35,7 +35,7 @@ const prices: Price[] = [
       quarterly: 0,
       yearly: 0,
     },
-    features: ["每小时 10 次免费问答", "每天 1 次 GPT-4 免费问答"],
+    features: ["每小时 10 次免费问答"],//, "每天 1 次 GPT-4 免费问答"
   },
   {
     name: "Pro",
@@ -44,7 +44,7 @@ const prices: Price[] = [
       quarterly: 79,
       yearly: 259,
     },
-    features: ["每三小时 50 次 GPT-3.5 问答", "每天 3 次 GPT-4 免费问答"],
+    features: ["每三小时 50 次 GPT-3.5 问答"],//, "每天 3 次 GPT-4 免费问答"
   },
   {
     name: "Premium",
@@ -54,9 +54,7 @@ const prices: Price[] = [
       yearly: 999,
     },
     features: [
-      "New Bing 免费",
-      "GPT-3.5 无限制问答",
-      "每天 10 次 GPT-4 免费问答",
+      "GPT-3.5 无限制问答",//"New Bing 免费","每天 10 次 GPT-4 免费问答",
     ],
   },
 ];
